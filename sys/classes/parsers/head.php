fb677ee27eb03765cecc7b7df802e1397d484576
Martin Waury <ich_bins@gmx.de>
2014-01-28 01:17:01 +0100

    fixed headline parser. only convert "+" wich appear as first element of an line

--- sys/classes/parsers/head.php
+++ sys/classes/parsers/head.php
@@ -15,7 +15,7 @@ class head extends abstractParser
     public function parseContent()
     {
         for ($i = 10; $i > 0; $i--) {
-            $this->replaceContentElement('/' . str_repeat('\+', $i) . '(.*)/', '<h' . $i . '>$1</h' . $i . '>');
+            $this->replaceContentElement('/^' . str_repeat('\+', $i) . '(.*)$/m', '<h' . $i . '>$1</h' . $i . '>');
         }
     }
 }
