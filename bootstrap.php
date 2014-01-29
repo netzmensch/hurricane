775a815cd224dd3841e56d4dc86ded41f3161de2
Martin Waury <ich_bins@gmx.de>
2014-01-30 00:42:38 +0100

    prepared bootstrap.php for parallel use in prod and test environment

---
+++ bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+namespace redcross\hurricane;
+
+define('CONFIG_PATH_CLASSES', 'sys/classes');
+define('CONFIG_PATH_PARSERS', 'sys/classes/parsers');
+define('CONFIG_PATH_TEMPLATES', 'res/template');
+define('CONFIG_PATH_CONTENT', 'page');
+define('CONFIG_APP_STARTPAGE', 'home');
+define('CONFIG_APP_WRONG_PATH_MESSAGE', 'wrong path...');
+
+function autoLoad($class)
+{
+    $folders = array(
+        CONFIG_PATH_CLASSES,
+        CONFIG_PATH_PARSERS,
+    );
+    $nameSpaceParts = explode('\\', $class);
+
+    foreach ($folders as $folder) {
+        $fullPath = __DIR__ . '/' . $folder . '/' . end($nameSpaceParts) . '.php';
+        if(file_exists($fullPath)) {
+            require $fullPath;
+        }
+    }
+}
+
+spl_autoload_register(__NAMESPACE__ . '\autoLoad');
