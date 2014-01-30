0573b0fcf1c15f96bde3b9bf497c9f8d392155ee
Martin Waury <ich_bins@gmx.de>
2014-01-30 01:30:13 +0100

    added content parser test

--- tests/classes/contentTest.php
+++ tests/classes/contentTest.php
@@ -5,6 +5,10 @@ use redcross\hurricane\classes\content;
 
 class contentTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers \redcross\hurricane\tests\classes\content::setContent
+     * @covers \redcross\hurricane\tests\classes\content::getContent
+     */
     public function testContentGetterAndSetter()
     {
         $content = new content('');
@@ -13,5 +17,20 @@ class contentTest extends \PHPUnit_Framework_TestCase
         $content->setContent($testContent);
 
         $this->assertEquals($testContent, $content->getContent());
+    }
+
+    /**
+     * @covers \redcross\hurricane\tests\classes\content::getParsers
+     */
+    public function testParseContent()
+    {
+        $sourceContent = file_get_contents(__DIR__ . '/../fixtures/content.txt');
+        $parsedContent = file_get_contents(__DIR__ . '/../fixtures/parsedContent.html');
+
+        $contentObject = new content($sourceContent);
+
+        $contentObject->parseContent();
+
+        $this->assertEquals($parsedContent, $contentObject->getContent());
     }
 }
